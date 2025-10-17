2e38e51be59192745ddd262e41a429d7071c974a
Oleksandr Honchar <figules@gmail.com>
2025-10-17 10:40:44 +0200

    fix password length and getNoteById function errors

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -35,7 +35,7 @@ export const getAllNotes = async (req, res, next) => {
 
 export const getNoteById = async (req, res, next) => {
   const { noteId } = req.params;
-  const note = await Note.findById({
+  const note = await Note.findOne({
     _id: noteId,
     userId: req.user._id,
   });
